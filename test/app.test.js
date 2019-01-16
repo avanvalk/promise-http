b668be2767ba0b01bc108d9713f206cc7c389a2e
avanvalk <drewvv@gmail.com>
2019-01-15 16:57:50 -0800

    added bodyParser.test and bodyParser

--- test/app.test.js
+++ test/app.test.js
@@ -4,9 +4,11 @@ const app = require('../lib/app');
 describe('app', () => {
   it('has a tester route', () => {
     return request(app)
+      // add .post and .set
       .get('/tester')
+      .query({ name: 'ryan' })
       .then(res => {
-        expect(res.body).toEqual({ testing: 123 });
+        expect(res.body).toEqual({ hi: 'there ryan' });
       });
   });
 });
